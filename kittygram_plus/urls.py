cb1297d31aa5cccf11b1c07f1c7433c529786c8a
Александр Сисецкий <alexandernice777@yandex.ru>
2023-02-18 22:36:34 +0300

    next hw

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,12 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet, OwnerViewSet
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('cats/recent-white-cats', CatViewSet)
+router.register(r'mycats', LightCatViewSet)
 router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
